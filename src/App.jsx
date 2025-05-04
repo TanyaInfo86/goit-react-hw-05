861b3e953375fbdef4ca30391f3cf80db04004f9
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-05-04 11:39:46 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// import { Routes, Route } from 'react-router-dom';
-// import HomePage from './pages/HomePage/HomePage';
-// import MoviesPage from './pages/MoviesPage/MoviesPage';
-// import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
-// import MovieCast from './components/MovieCast/MovieCast';
-// import MovieReviews from './components/MovieReviews/MovieReviews';
-// import Navigation from './components/Navigation/Navigation';
-// import styles from './App.module.css'; 
-
-// export default function App() {
-//     return (
-//         <div className={styles.wrapper}>
-//             <Navigation />
-//             <Routes>
-//                 <Route path="/" element={<HomePage />} />
-//                 <Route path="/movies" element={<MoviesPage />} />
-//                 <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-//                     <Route path="cast" element={<MovieCast />} />
-//                     <Route path="reviews" element={<MovieReviews />} />
-//                 </Route>
-//                 <Route path="*" element={<div className={styles.loading}>404 Not Found</div>} />
-//             </Routes>
-//         </div>
-//     );
-// }
-
-
 import { Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Navigation from './components/Navigation/Navigation';
